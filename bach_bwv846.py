bf2a0c1ae02e4bc23a15a53c7be2a71c756f497e
hhyyg <hiroka.yago@gmail.com>
2018-12-16 16:28:15 +0900

    init

---
+++ bach_bwv846.py
@@ -0,0 +1,33 @@
+#%%
+from music21 import note,stream,corpus,chord,environment,converter,midi
+from pyknon.genmidi import Midi
+from pyknon.music import NoteSeq, Note
+
+#%%
+bwv846 = corpus.parse('bach/bwv846')
+bwv846.measures(1, 2).show()
+
+#%%
+bwv846.show('midi')
+
+#%%
+bwv846.parts[0].plot()
+bwv846.parts[1].plot()
+
+#%%
+bwv846.plot('histogram', 'pitch')
+
+#%%
+bwv846.plot('histogram', 'pitchClass')
+
+#%%
+bwv846.plot('3dbars', 'pitchClass')
+
+#%%
+bwv846.parts[0].plot('histogram', 'quarterLength', title='right hand')
+bwv846.parts[1].plot('histogram', 'quarterLength', title='left hand')
+
+#%%
+f = bwv846.parts[0].flat
+note16count = [note for note in f.notes if note.quarterLength == 0.25]
+len(note16count)
---
+++ main.py
@@ -0,0 +1,22 @@
+#%%
+from music21 import note,stream,corpus,chord,environment,converter,midi
+from pyknon.genmidi import Midi
+from pyknon.music import NoteSeq, Note
+
+#%%
+stream1 = stream.Stream()
+
+note = note.Note("C4", quarterLength = 1)
+stream1.repeatAppend(note, 4)
+stream1.show()
+
+#%%
+stream1.show('midi')
+
+#%% 
+s = converter.parse('tinyNotation: 4/4 e8 e8 r e8 r c8 e8 r g4 r G4 r')
+s.show()
+
+
+
+
